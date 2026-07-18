595782ce598c96e7a633957cf63b65df1f75e04b
MishkaKovalQA <mishka.koval.qa@gmail.com>
2026-07-18 18:35:28 +0500

    change type for title to VARCHAR

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -12,9 +12,9 @@ const schema = `
 CREATE TABLE IF NOT EXISTS scheduler (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	date CHAR(8) NOT NULL DEFAULT '',
-	title TEXT NOT NULL DEFAULT '',
+	title VARCHAR NOT NULL DEFAULT '',
 	comment TEXT NOT NULL DEFAULT '',
-	repeat VARCHAR(128) NOT NULL DEFAULT ''
+	repeat VARCHAR NOT NULL DEFAULT ''
 );
 
 CREATE INDEX IF NOT EXISTS scheduler_date ON scheduler(date);
